bbac5ca5b67e2102757ecb002b6d3fd0050495f4
Peter Hencz <peter.hencz@gmail.com>
2020-05-09 23:16:20 +0200

    routing szopodas

--- site/src/App.js
+++ site/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Home from "./Home";
 import Info from "./Info";
 import Work from "./Work";
@@ -8,45 +8,49 @@ import "./App.scss";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <Cursor>
-      <div className="App">
-        <Cursor></Cursor>
-        <Router>
-          <div className="Page">
-            <nav className="navigation">
-              <p>
-                <Link className="link" to="/work">
-                  WORK
-                </Link>
-              </p>
-              <p>
-                <Link className="link" to="/">
-                  HOME
-                </Link>
-              </p>
-              <p>
-                <Link className="link" to="/info">
-                  INFO
-                </Link>
-              </p>
-            </nav>
+class App extends Component {
+  state = {
+    class: "menu_photo",
+  };
 
-            {/* A <Switch> looks through its children <Route>s and
+  render() {
+    return (
+      <Cursor>
+        <div className="App">
+          <Cursor></Cursor>
+          <Router>
+            <div className="Page">
+              <nav className="navigation">
+                <p>
+                  <Link className="link" to="/work">
+                    WORK
+                  </Link>
+                </p>
+                <p>
+                  <Link className="link" to="/">
+                    HOME
+                  </Link>
+                </p>
+                <p>
+                  <Link className="link" to="/info">
+                    INFO
+                  </Link>
+                </p>
+              </nav>
+
+              {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/work">
-                <Work></Work>
-              </Route>
-              <Route path="/info">{Info}</Route>
-              <Route path="/">{Home}</Route>
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    </Cursor>
-  );
+              <Switch>
+                <Route path="/work">{Work}</Route>
+                <Route path="/info">{Info}</Route>
+                <Route path="/">{Home}</Route>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </Cursor>
+    );
+  }
 }
 
 export default App;
--- site/src/Work.js
+++ site/src/Work.js
@@ -7,6 +7,7 @@ import {
   Route,
   Link,
   Redirect,
+  useParams,
 } from "react-router-dom";
 import LazyLoad from "react-lazy-load";
 import Resin from "./projects/Resin";
@@ -18,43 +19,79 @@ import resin1 from "./assets/photos/resin1.jpg";
 import hurka1 from "./assets/photos/hurka1.jpg";
 import bauxit1 from "./assets/photos/bauxit1.jpg";
 
+const IMAGES = [
+  { id: 0, title: "Dark Orchid", color: "DarkOrchid" },
+  { id: 1, title: "Lime Green", color: "LimeGreen" },
+  { id: 2, title: "Tomato", color: "Tomato" },
+  { id: 3, title: "Seven Ate Nine", color: "#789" },
+  { id: 4, title: "Crimson", color: "Crimson" },
+];
+
+function Image({ color }) {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: 400,
+        background: color,
+      }}
+    />
+  );
+}
+
+function ImageView() {
+  let { id } = useParams();
+  let image = IMAGES[parseInt(id, 10)];
+
+  if (!image) return <div>Image not found</div>;
+
+  return (
+    <div>
+      <h1>{image.title}</h1>
+      <Image color={image.color} />
+    </div>
+  );
+}
+
 function Work() {
   return (
-    <div className="Work">
-      <Router>
-        <NavLink to="/resin">
-          <LazyLoad
-            width={"30vw"}
-            heigth={"auto"}
-            debounce={false}
-            offsetVertical={500}>
-            <img className="menu_photo" alt="resin" src={resin1}></img>
-          </LazyLoad>
-        </NavLink>
-        <Link to="/hurka">
-          <LazyLoad
-            width={"30vw"}
-            heigth={"auto"}
-            debounce={false}
-            offsetVertical={500}>
-            <img className="menu_photo" alt="hurka" src={hurka1}></img>
-          </LazyLoad>
-        </Link>
-        <Link to="/bauxit">
-          <LazyLoad
-            width={"30vw"}
-            heigth={"auto"}
-            debounce={false}
-            offsetVertical={500}>
-            <img className="menu_photo" alt="bauxit" src={bauxit1}></img>
-          </LazyLoad>
-        </Link>
-        <Switch>
-          <Route path="/resin">{Resin}</Route>
-          <Route path="/bauxit">{Bauxit}</Route>
-          <Route path="/hurka">{Hurka}</Route>
-        </Switch>
-      </Router>
+    <div className="Main">
+      <div className="Work">
+        <Router>
+          <Link to="/work/resin">
+            <LazyLoad
+              width={"30vw"}
+              heigth={"auto"}
+              debounce={false}
+              offsetVertical={500}>
+              <img className="menu_photo" alt="resin" src={resin1}></img>
+            </LazyLoad>
+          </Link>
+          <Link to="/work/hurka">
+            <LazyLoad
+              width={"30vw"}
+              heigth={"auto"}
+              debounce={false}
+              offsetVertical={500}>
+              <img className="menu_photo" alt="hurka" src={hurka1}></img>
+            </LazyLoad>
+          </Link>
+          <Link to="/work/bauxit">
+            <LazyLoad
+              width={"30vw"}
+              heigth={"auto"}
+              debounce={false}
+              offsetVertical={500}>
+              <img className="menu_photo" alt="bauxit" src={bauxit1}></img>
+            </LazyLoad>
+          </Link>
+          <Switch>
+            <Route path="/work/resin">{Resin}</Route>
+            <Route path="/work/bauxit">{Bauxit}</Route>
+            <Route path="/work/hurka">{Hurka}</Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
